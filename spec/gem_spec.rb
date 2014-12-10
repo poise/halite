920f03565ef2625bb7cf964f16e746d128963ee8
Noah Kantrowitz <noah@coderanger.net>
2014-12-10 00:14:01 -0800

    More tests for Gem.

--- spec/gem_spec.rb
+++ spec/gem_spec.rb
@@ -60,4 +60,45 @@ describe Halite::Gem do
       end
     end
   end # /context when loading test1
+
+  context 'when loading test2' do
+    let(:gem_name) { 'test2' }
+    its(:name) { is_expected.to eq 'test2' }
+    its(:version) { is_expected.to eq '4.5.6' }
+    its(:files) { is_expected.to include 'test2.gemspec' }
+    its(:files) { is_expected.to include 'lib/test2.rb' }
+    its(:license_header) { is_expected.to eq "# coding: utf-8\n" }
+    its(:each_library_file) { is_expected.to eq [
+      [File.expand_path('../data/gems/test2/lib/test2.rb', __FILE__), 'test2.rb'],
+      [File.expand_path('../data/gems/test2/lib/test2/resource.rb', __FILE__), 'test2/resource.rb'],
+      [File.expand_path('../data/gems/test2/lib/test2/version.rb', __FILE__), 'test2/version.rb'],
+    ] }
+
+    describe '#each_file' do
+      context 'with no prefixes' do
+        it 'returns all files' do
+          expect(subject.each_file).to eq [
+            [File.expand_path('../data/gems/test2/chef/attributes.rb', __FILE__), 'chef/attributes.rb'],
+            [File.expand_path('../data/gems/test2/chef/recipes/default.rb', __FILE__), 'chef/recipes/default.rb'],
+            [File.expand_path('../data/gems/test2/chef/templates/default/conf.erb', __FILE__), 'chef/templates/default/conf.erb'],
+            [File.expand_path('../data/gems/test2/lib/test2.rb', __FILE__), 'lib/test2.rb'],
+            [File.expand_path('../data/gems/test2/lib/test2/resource.rb', __FILE__), 'lib/test2/resource.rb'],
+            [File.expand_path('../data/gems/test2/lib/test2/version.rb', __FILE__), 'lib/test2/version.rb'],
+            [File.expand_path('../data/gems/test2/test2.gemspec', __FILE__), 'test2.gemspec'],
+          ]
+        end
+      end
+
+      context 'with a prefix of chef' do
+        it 'returns only files in that folder' do
+          expect(subject.each_file('chef')).to eq [
+            [File.expand_path('../data/gems/test2/chef/attributes.rb', __FILE__), 'attributes.rb'],
+            [File.expand_path('../data/gems/test2/chef/recipes/default.rb', __FILE__), 'recipes/default.rb'],
+            [File.expand_path('../data/gems/test2/chef/templates/default/conf.erb', __FILE__), 'templates/default/conf.erb'],
+          ]
+        end
+      end
+    end
+  end # /context when loading test2
+
 end
